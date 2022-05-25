b5d009dc14f514246d5b6b60585778f01cc8df01
Remant <remantmahato9798@gmail.com>
2022-05-25 08:54:14 +0530

    [Remant] Added : Entity framework to connect to sql database

---
+++ FundooNotes/RepositoryLayer/Services/UserRL.cs
@@ -0,0 +1,16 @@
+﻿using DataBaseLayer.Users;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class UserRL : IUserRL
+    {
+        public void AddUser(UserPostModel userPostModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
